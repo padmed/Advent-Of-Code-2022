6b7edb4f60d93f909606016ace31f37d6877a463
padmed <ddarchinian@gmail.com>
2022-12-25 15:46:47 +0400

    Day 1 solution

---
+++ Day_1/day1.py
@@ -0,0 +1,19 @@
+elve_calories = []
+with open('input.txt') as elves:
+    calories = elves.readlines()
+
+    elve_calorie = 0
+    for i in range(len(calories)):
+        calorie = calories[i].strip()
+
+        if calorie != '':
+            elve_calorie += int(calorie)
+        else:
+            elve_calories.append(elve_calorie)
+            elve_calorie = 0
+
+
+#Answer
+most_calorie = max(elve_calories)
+print(most_calorie)
+
