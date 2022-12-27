5b03eb4784b9d63c2c39e3ee0ea9a49ca2fe3ae8
padmed <ddarchinian@gmail.com>
2022-12-27 22:20:39 +0400

    Day 4 solution

---
+++ Day_4/solution.py
@@ -0,0 +1,48 @@
+class SectionPairs():
+    def __init__(self):
+        self.sectionPairs = self.getSectionPairs()
+    
+    def getSectionPairs(self):
+        sectionPairs = []
+        with open('input.txt') as Pairs:
+            for pair in Pairs:
+                sectionPairs.append(pair.strip())
+        
+        return sectionPairs
+    
+    def rangeToList(self, sectionRange):
+        start, end = sectionRange.split('-')
+        start, end = int(start), int(end)
+        
+        rangeList = []
+        for section in range(start, end + 1):
+            rangeList.append(section)
+
+        return rangeList
+
+    def checkFaultyPairs(self, a, b):
+        if len(a) < len(b):
+            a, b = b, a
+        
+        for section in b:
+            if section not in a:
+                return False
+        return True
+
+    def countFaultyPairs(self):
+        faultyCount = 0
+        for pair in self.sectionPairs:
+            pair_a, pair_b = pair.split(',')
+            pair_a, pair_b = self.rangeToList(pair_a), self.rangeToList(pair_b)
+            
+            if self.checkFaultyPairs(pair_a, pair_b):
+                faultyCount += 1
+        
+        return faultyCount
+
+            
+
+
+
+solution = SectionPairs()
+print(solution.countFaultyPairs())
